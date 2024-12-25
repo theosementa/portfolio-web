ff0bd80bcf81ef05cd5cf231466f586a7eef7cfc
Theo Sementa <theosementa@gmail.com>
2024-12-25 12:42:45 +0100

    feat: update base

--- vite.config.ts
+++ vite.config.ts
@@ -4,7 +4,7 @@ import { defineConfig } from 'vite'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: "/portfolio",
+  base: "/portfolio-web",
   server: {
     port: 5006,
     strictPort: true,
